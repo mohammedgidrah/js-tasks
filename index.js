348d2ed5055aad57b40f6f49cf1895083d861632
mohammedgidrah <m7mgidrah@gmail.com>
2024-06-03 01:42:22 +0300

    js

---
+++ index.js
@@ -0,0 +1,172 @@
+// task 1
+let cach;
+let currentliabilities;
+cach = 1000;
+currentliabilities = 500;
+let result1;
+result1 = cach / currentliabilities;
+console.log(" cach =1000");
+console.log(" current liabilities=500");
+console.log(" cach/current liabilities");
+console.log(" the output =" + result1);
+console.log("..........................................")
+let revenues;
+let expenses;
+revenues = 1000;
+expenses = 500;
+let result2;
+result2 = cach - currentliabilities;
+console.log(" revenues =1000");
+console.log(" expenses=500");
+console.log(" revenues - expenses");
+console.log(" the output =" + result2);
+console.log("..........................................")
+let liabilities;
+let equity;
+liabilities = 1000;
+equity = 500;
+let result3;
+result3 = cach + currentliabilities;
+console.log(" liabilities =1000");
+console.log(" equity=500");
+console.log(" liabilities + equity");
+console.log(" the output =" + result3);
+console.log("..........................................")
+let profit;
+let sales;
+profit = 1000;
+sales = 500;
+let result4;
+result4 = cach * currentliabilities;
+console.log(" profit =1000");
+console.log(" sales=500");
+console.log(" profit * sales");
+console.log(" the output =" + result4);
+console.log("..........................................")
+let num1;
+let num2;
+let num3;
+num1 = 7;
+num2 = 9;
+num3 = 2;
+let result5;
+let avarege;
+result5 = num1 + num2 + num3;
+avarege = result5 / 3;
+console.log(" num1 = 7");
+console.log(" num2 = 9");
+console.log(" num3 = 2");
+console.log(" num1 + num2 + num3");
+console.log(" avarege=result5/3");
+console.log(" the output =" + avarege);
+console.log(".........................................")
+let price;
+let discount;
+price = 150;
+discount = 45;
+let result6;
+result6 = price - discount;
+console.log(" price =150");
+console.log(" discount=30%");
+console.log(" price - discount");
+console.log(" the output =" + result6);
+console.log("..........................................")
+let age = 20;
+
+if (age > 18 && age < 30) {
+    console.log("true");
+}
+else {
+    console.log("false")
+}
+console.log(".........................................")
+let number1;
+let number2;
+number1 = 2;
+number2 = 3;
+let result7;
+result7 = number1 ** number2;
+console.log(" the output =" + result7);
+console.log(".........................................")
+let number3;
+let number4;
+number3 = 10;
+number4 = 4;
+let result8;
+result8 = number3 / number4;
+result8 = (Math.floor(result8))
+console.log(" the output =" + result8);
+// task 2
+console.log(".........................................")
+console.log(typeof (100));
+console.log(typeof (73.9));
+console.log(typeof (NaN));
+console.log(typeof ("water"));
+console.log(typeof (false));
+console.log(typeof 9 != 11);
+console.log("Orang" + "e");
+console.log("Orange" - "s");
+console.log("4" + "8");
+console.log("4" - "8");
+console.log("name" - 3);
+console.log("name" - 3);
+console.log(82 * "word");
+console.log(1 + " hello");
+console.log("hello " + 1);
+console.log(1 + true);
+console.log("hello " + true);
+console.log(typeof (Infinity));
+console.log(1 == '1');
+console.log(1 === '1');
+// task 3
+console.log(".........................................")
+let text;
+text = "Welcome to Orange";
+console.log(text[8].toUpperCase() + text[9].toUpperCase());
+console.log(text.replace("Welcome to", "Hello from"));
+console.log(text.toLowerCase());
+console.log(text.length);
+console.log(text + " jordan");
+console.log(".........................................")
+// task 4
+alert("welcome ");
+// task7
+var a = 10;
+var b = 5
+console.log("Addition:" + a + b);
+console.log("Subtraction:" + a - b);
+console.log("Multiplication:" + a * b);
+console.log("Division:" + a / b);
+console.log("Modulus (remainder):" + a % b);
+console.log(".........................................")
+console.log("Addition:" + a > b);
+console.log("Subtraction:" + a < b);
+console.log("Multiplication:" + a == b);
+console.log("Division:" + a == b);
+console.log("Modulus (remainder):" + a != b);
+console.log(".........................................")
+var x = true;
+var y = false;
+console.log("AND:" + x && y);
+console.log("OR:" + x || y);
+console.log("NOT:" + !x && !y);
+console.log(".........................................")
+console.log("Add and assign:" + (a += 5));
+console.log("Subtract and assign:" + (a -= 5));
+console.log("Multiplay and assign:" + (a *= 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
