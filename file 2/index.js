dc080f7729cd520585746e6e27514aec2b070d23
mohammedgidrah <m7mgidrah@gmail.com>
2024-06-05 01:58:47 +0300

    js

--- file 2/index.js
+++ file 2/index.js
@@ -1,96 +1,97 @@
-// task 1
-for (let i = 1; i <= 10; i++) {
-    document.writeln(i)
-}
-// task 2
-document.write("<br/>")
-let x = 1;
-while (x <= 10) {
-    document.writeln(x)
-    x++;
-}
-// task 3
-document.write("<br/>")
-for (let y = 0; y <= 10; y = y + 2) {
-    document.writeln(y)
-}
-// task 4
-document.write("<br/>")
-var sum = 0;
-for (var a = 1; a <= 10; a++) {
-    sum = sum + a;
-}
-document.write(sum)
-// task 5
-document.write("<br/>")
-for (let y = 0; y <= 10; y++) {
-    if (y % 2 != 0) {
+    // task 1
+    for (let i = 1; i <= 10; i++) {
+        document.writeln(i)
+    }
+    // task 2
+    document.write("<br/>")
+    let x = 1;
+    while (x <= 10) {
+        document.writeln(x)
+        x++;
+    }
+    // task 3
+    document.write("<br/>")
+    for (let y = 0; y <= 10; y = y + 2) {
         document.writeln(y)
-
     }
-}
-// task 6
-document.write("<br/>")
-
-let age = 27;
-if (age > 16) {
-    document.write("You are an adult")
-
-}
-// task 7
-document.write("<br/>")
-var score = 45;
-if (score <= 50) {
-    document.write("You failed the test")
-}
-// task 8
-document.write("<br/>")
-var Name = "john";
-if (Name == "john") {
-    document.write("Hello " + Name)
-}
-// task 9
-document.write("<br/>")
-var day = "Monday";
-if (day == "Monday") {
-    document.write("It's a weekday")
-}
-// task 10
-document.write("<br/>")
-var num = 4;
-if (num % 2 == 0) {
-    document.write("The number is even")
-}
-// task 11
-document.write("<br/>")
-var x1 = 5;
-if (x1 >= 1) {
-    document.write("x1 is a positive number")
-}
-// task 12
-document.write("<br/>")
-var y = -5;
-if (y < 0) {
-    document.write("y is a negative  number")
-}
-// task 13
-document.write("<br/>")
-var gpa = 3.5;
-if (gpa > 3.0) {
-    document.write("Congratulations, you have a good GPA!")
-}
-// task 14
-document.write("<br/>")
-var numbers=10;
-    if (numbers %= 2) {
-        document.write("true")
+    // task 4
+    document.write("<br/>")
+    var sum = 0;
+    for (var a = 1; a <= 10; a++) {
+        sum = sum + a;
     }
-    for(let i=2; i<=2;i++){
-        if(numbers %2 ==0){
-            document.write("flase")
+    document.write(sum)
+    // task 5
+    document.write("<br/>")
+    for (let y = 0; y <= 10; y++) {
+        if (y % 2 != 0) {
+            document.writeln(y)
+
         }
     }
-    
+    // task 6
+    document.write("<br/>")
+
+    let age = 27;
+    if (age > 16) {
+        document.write("You are an adult")
+
+    }
+    // task 7
+    document.write("<br/>")
+    var score = 45;
+    if (score <= 50) {
+        document.write("You failed the test")
+    }
+    // task 8
+    document.write("<br/>")
+    var Name = "john";
+    if (Name == "john") {
+        document.write("Hello " + Name)
+    }
+    // task 9
+    document.write("<br/>")
+    var day = "Monday";
+    if (day == "Monday") {
+        document.write("It's a weekday")
+    }
+    // task 10
+    document.write("<br/>")
+    var num = 4;
+    if (num % 2 == 0) {
+        document.write("The number is even")
+    }
+    // task 11
+    document.write("<br/>")
+    var x1 = 5;
+    if (x1 >= 1) {
+        document.write("x1 is a positive number")
+    }
+    // task 12
+    document.write("<br/>")
+    var y = -5;
+    if (y < 0) {
+        document.write("y is a negative  number")
+    }
+    // task 13
+    document.write("<br/>")
+    var gpa = 3.5;
+    if (gpa > 3.0) {
+        document.write("Congratulations, you have a good GPA!")
+    }
+    // task 14
+    document.write("<br/>")
+    var numbers=10;
+        if (numbers %= 2) {
+            document.write("true")
+        }
+        for(let i=2; i<=2;i++){
+            if(numbers %2 ==0){
+                document.write("flase")
+            }
+        }
+        
+        
 
 
 
---
+++ file 3/index.js
@@ -0,0 +1,52 @@
+// task 1
+function sayHello(Name) {
+    var Name = prompt("enter your name")
+    document.writeln("Hello, " + Name +"<br/>");
+}
+sayHello();
+// task 2
+var square = function (numbers) {
+    return numbers ** 2;
+
+}
+console.log(square(5));
+// task 3
+let multiply = (num1, num2) => num1 * num2;
+console.log(multiply(5, 2));
+// task 4
+const car = {
+    make: 'toyota', model: 'camry', year: '(2017)',
+    getCarInfo: function () {
+        console.log(car.make + " " + car.model + " " + car.year)
+    }
+
+
+}
+car.getCarInfo();
+// task 5
+const colors = ["red", "blue", "green"];
+function print() {
+    for (let i = 0; i < 3; i++) {
+        document.writeln(colors[i] + "<br/>")
+    }
+}
+print()
+// task 6
+function getStringLength(str) {
+    return str.length;
+}
+console.log(getStringLength('Hello'));
+console.log(getStringLength('JavaScript'));
+// task 7
+function toUpperCase(str) {
+    return str.toUpperCase();
+}
+console.log(toUpperCase('hello'));
+console.log(toUpperCase('world'));
+// task 8
+function split(str,delimiter){
+    var array=str.split(delimiter);
+    return array;
+}
+console.log(split('Hello World', ' '));
+console.log(split('JavaScript is fun', ' '))
