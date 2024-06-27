4909313567557b52fd8d28a92aa98b42219eac0b
mohammedgidrah <m7mgidrah@gmail.com>
2024-06-27 09:09:32 +0300

    js

--- api in js/index.js
+++ api in js/index.js
@@ -1,25 +1,27 @@
 // // task 1
 
-// fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+
 //     .then(response => {
 //         return response.json();
 //     })
-//     .then((data) => {
-//         const dataContainer = document.getElementById('task1');
-//         data.forEach(element => {
-//             const divcontent = document.createElement('div');
-//             divcontent.className = 'item';
-//             divcontent.innerHTML = `
-//                         <h3> titel : ${element.title}</h3>
-//                         <p> body : ${element.body}</p>
-                        
-//                         `;
-//             dataContainer.appendChild(divcontent);
-//         })
+// .then((data) => {
+//     const dataContainer = document.getElementById('task1');
+//     data.forEach(element => {
+//         const divcontent = document.createElement('div');
+//         divcontent.className = 'item';
+//         divcontent.innerHTML = `
+//                     <h3> titel : ${element.title}</h3>
+//                     <p> body : ${element.body}</p>
+
+//                     `;
+//         dataContainer.appendChild(divcontent);
 //     })
-//     .catch(error => {
-//         console.error('There was a problem with the fetch operation:', error);
-//     });
+// })
+// .catch(error => {
+//     console.error('There was a problem with the fetch operation:', error);
+// });
 // // task 2
 
 // fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
@@ -30,12 +32,12 @@
 //         const dataContainer = document.getElementById('task2');
 //         data.forEach(element => {
 //             const divcontent = document.createElement('div');
-//             divcontent.className = 'item';
+//             // divcontent.className = 'item';
 //             divcontent.innerHTML = `
 //             <h4> name  : ${element.name}</h4>
 //             <p> email : ${element.email}</p>
 //             <p> body : ${element.body}</p>
-            
+
 //             `;
 //             dataContainer.appendChild(divcontent);
 //         })
@@ -128,82 +130,63 @@
 //         console.error('There was a problem with the fetch operation:', error);
 //     });
 
+
+
 // bonus task
-const post = fetch(`https://jsonplaceholder.typicode.com/posts`).then(response => response.json());
-const comments = fetch(`https://jsonplaceholder.typicode.com/comments`).then(response => response.json());
-const users = fetch(`https://jsonplaceholder.typicode.com/users`).then(response => response.json());
 
-Promise.all([post, comments,users])
-.then(([data1, data2,data3]) => {
-    // console.log(data1)
-    // console.log(data2)
-    // console.log(data3)
-    return data1, data2 ,data3;
-  })
+// const post = fetch(`https://jsonplaceholder.typicode.com/posts`).then(response => response.json());
+// const comments = fetch(`https://jsonplaceholder.typicode.com/comments`).then(response => response.json());
+// const users = fetch(`https://jsonplaceholder.typicode.com/users`).then(response => response.json());
 
-
-    .then((data) => {
-        document.getElementById("fetchData").addEventListener("click", () => {
-
-            const dataContainer = document.getElementById('post_content');
-            data.forEach(element => {
-                const divcontent = document.createElement('div');
-                divcontent.className = 'item';
-                divcontent.innerHTML = `
-                <h3> username : ${element.username}</h3>
-                <p> email : ${element.email}</p>
-                <p> address : ${element.address.city} , ${element.address.suite}, ${element.address.city}, ${element.address.zipcode}</p>
-                `;
-                dataContainer.appendChild(divcontent);
-            })
-        });
-
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const apiUrl = "https://api.github.com/users/mohammedgidrah/repos";
-// fetch(apiUrl)
-//     .then(response => {
-//         return response.json();
+// Promise.all([post, comments, users])
+//     .then(([data1, data2, data3]) => {
+//         // console.log(data1)
+//         // console.log(data2)
+//         // console.log(data3)
+//         return data1, data2, data3;
 //     })
-//     .then((data) => {
-//         // document.getElementById("fetchData").addEventListener("click" ,()=>{  
-//         const dataContainer = document.getElementById('hi');
-//         data.forEach(element => {
-//             console.log(typeof (element));
-//             const itemDiv = document.createElement('div');
-//             itemDiv.className = 'item';
-//             itemDiv.innerHTML = `
-//                     <img src=${element.owner.avatar_url} width=50px higth=50px>
-//                         <h2>repo name : ${element.name}</h2>
-//                         <p>repo id : ${element.id}</p>
-//                         <p>repo owner name : ${element.owner.login}</p>
-//                         <a href=${element.owner.html_url} target="_blank">github link</a>
-//                     `;
-//             dataContainer.appendChild(itemDiv);
-//         })
-//         // });
+
+
+//     .then((data1 ,data2 ) => {
+//         document.getElementById("fetchData").addEventListener("click", () => {
+
+//             const dataContainer = document.getElementById('post_content');
+//             data1.forEach(element => {
+//                 const divcontent = document.createElement('div');
+//                 divcontent.className = 'item';
+//                 divcontent.innerHTML = `
+//                 <h3> username : ${element.username}</h3>
+//                 <p> email : ${element.email}</p>
+//                 <p> address : ${element.address.city} , ${element.address.suite}, ${element.address.city}, ${element.address.zipcode}</p>
+//                 `;
+//                 dataContainer.appendChild(divcontent);
+//             })
+//         });
+
 //     })
 //     .catch(error => {
 //         console.error('There was a problem with the fetch operation:', error);
 //     });
+
+// using async and await
+
+// async function getJson1() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await response.json()
+//     data.forEach(element => {
+//         const contaner = document.getElementById('task1')
+//         const divcontent = document.createElement('div');
+//         divcontent.innerHTML = `
+//         <h1>title: ${element.title}</h1>
+//         `;
+//         contaner.appendChild(divcontent);
+//     })
+
+
+// }
+// getJson1();
+
+
+
+
+
